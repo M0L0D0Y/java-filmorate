e74833d56b596c778ffcb7c756104f5fe924bf74
M0L0D0Y <i7ascha@gmail.com>
2022-06-13 13:49:42 +0300

    refactor: исправил класс FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,8 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
@@ -10,24 +10,20 @@ import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
 
 public class FilmController {
     private final FilmService filmService;
+    private final FilmStorage memoryFilmStorage;
 
-    @Autowired
-    public FilmController(FilmService filmService) {
+    public FilmController(FilmService filmService, FilmStorage memoryFilmStorage) {
         this.filmService = filmService;
+        this.memoryFilmStorage = memoryFilmStorage;
     }
-
-    private final FilmStorage memoryFilmStorage = InMemoryFilmStorage.getInMemoryFilmStorage();
 
     @GetMapping("/films")
     public Collection<Film> getAllFilms() {
@@ -40,24 +36,24 @@ public class FilmController {
     }
 
     @DeleteMapping(value = "/films")
-    public void deleteFilm(@RequestParam long id) throws ValidationException, NullPointerException  {
+    public void deleteFilm(@RequestParam long id) throws NotFoundException, ValidationException {
         memoryFilmStorage.deleteFilm(id);
     }
 
     @PutMapping(value = "/films")
-    public Film updateFilm(@Valid @RequestBody Film film) throws ValidationException, NullPointerException {
+    public Film updateFilm(@Valid @RequestBody Film film) throws ValidationException, NotFoundException {
         return memoryFilmStorage.updateFilm(film);
     }
 
     @PutMapping(value = "/films/{id}/like/{userId}")
     public void addLike(@PathVariable long id,
-                        @PathVariable long userId) throws ValidationException, NullPointerException  {
+                        @PathVariable long userId) throws NotFoundException, ValidationException {
         filmService.addLike(id, userId);
     }
 
     @DeleteMapping(value = "/films/{id}/like/{userId}")
     public void deleteLike(@PathVariable long id,
-                           @PathVariable long userId) throws ValidationException, NullPointerException  {
+                           @PathVariable long userId) throws NotFoundException, ValidationException {
         filmService.deleteLike(id, userId);
     }
 
